fd864580d7a69aaf8d2ca2951042c8a5521e6a4d
Seqret1337 <alexandernordvaag@gmail.com>
2024-10-04 15:54:02 +0200

    Edited splash screen so it looks different and includes three colors

--- game/splash.mjs
+++ game/splash.mjs
@@ -1,14 +1,13 @@
+import { ANSI } from "./ansi.mjs";
 
 const ART = `
- ______  ____   __      ______   ____    __      ______   ___     ___
-|      ||    | /  ]    |      | /    |  /  ]    |      | /   \\   /  _]
-|      | |  | /  /     |      ||  o  | /  /     |      ||     | /  [_
-|_|  |_| |  |/  /      |_|  |_||     |/  /      |_|  |_||  O  ||    _]
-  |  |   |  /   \\_       |  |  |  _  /   \\_       |  |  |     ||   [_
-  |  |   |  \\     |      |  |  |  |  \\     |      |  |  |     ||     |
-  |__|  |____\\____|      |__|  |__|__|\\____|      |__|   \\___/ |_____|
-
-`
+${ANSI.COLOR.BLUE}  _______   _____    ____   ${ANSI.COLOR.GREEN}  _______       ___       ____   ${ANSI.COLOR.YELLOW}  _______   ____    ______ ${ANSI.RESET}
+${ANSI.COLOR.BLUE} |__   __| |_   _|  / ___|  ${ANSI.COLOR.GREEN} |__   __|     / _ \\     / ___|  ${ANSI.COLOR.YELLOW} |__   __| / __ \\  |  ____|${ANSI.RESET}
+${ANSI.COLOR.BLUE}    | |      | |   / /      ${ANSI.COLOR.GREEN}    | |       / /_\\ \\   / /      ${ANSI.COLOR.YELLOW}    | |   | |  | | | |__   ${ANSI.RESET}
+${ANSI.COLOR.BLUE}    | |      | |  | |       ${ANSI.COLOR.GREEN}    | |      / _____ \\ | |       ${ANSI.COLOR.YELLOW}    | |   | |  | | |  __|  ${ANSI.RESET}
+${ANSI.COLOR.BLUE}    | |     _| |_  \\ \\___   ${ANSI.COLOR.GREEN}    | |     / /     \\ \\ \\ \\___   ${ANSI.COLOR.YELLOW}    | |   | |__| | | |____ ${ANSI.RESET}
+${ANSI.COLOR.BLUE}    |_|    |_____|  \\____|  ${ANSI.COLOR.GREEN}    |_|    /_/       \\_\\ \\____|  ${ANSI.COLOR.YELLOW}    |_|    \\____/  |______|${ANSI.RESET}
+`;
 
 function showSplashScreen() {
     console.log(ART);
